c1365f345c8298eb6084b8f343e74214c5d145ec
kevin <kmarpaung430@gmail.com>
2025-06-27 00:19:02 +0700

    add rensponsive

--- src/App.jsx
+++ src/App.jsx
@@ -292,7 +292,7 @@ export default function CarRentalLanding() {
           <div className="flex justify-between items-center h-16">
             <div className="flex items-center space-x-2">
               <Car className="h-8 w-8 text-blue-600" />
-              <span className="text-md font-bold bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">
+              <span className="text-2xl font-bold bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">
                 TarihoranRentalMobilMedan
               </span>
             </div>
@@ -622,9 +622,9 @@ export default function CarRentalLanding() {
         <div className="max-w-7xl mx-auto px-4">
           <div className="text-center mb-16">
             <h2 className="text-4xl font-bold text-gray-800 mb-4">
-              Mengapa Pilih{" "}
-              <span className="bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">
-                TarihoranRentalMobilMedan?
+              Mengapa Pilih
+              <span className="  bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">
+                TarihoranRental <br></br>MobilMedan?
               </span>
             </h2>
           </div>
