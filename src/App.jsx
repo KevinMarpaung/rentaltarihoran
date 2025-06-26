525c9fa3592eeb38b96ed0be3adc24af632c530c
kevin <kmarpaung430@gmail.com>
2025-06-26 23:42:58 +0700

    add rensponsive

--- src/App.jsx
+++ src/App.jsx
@@ -292,7 +292,7 @@ export default function CarRentalLanding() {
           <div className="flex justify-between items-center h-16">
             <div className="flex items-center space-x-2">
               <Car className="h-8 w-8 text-blue-600" />
-              <span className="text-2xl font-bold bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">
+              <span className="text-md font-bold bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">
                 TarihoranRentalMobilMedan
               </span>
             </div>
